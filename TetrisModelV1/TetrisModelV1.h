979b8ac2a8429222bfd453531ec736d6d9a4b033
Neil Gupta <nogupta@umass.edu>
2024-04-27 19:23:42 -0400

    preliminary model

--- TetrisModelV1/TetrisModelV1.h
+++ TetrisModelV1/TetrisModelV1.h
@@ -1,1 +1,11 @@
 #include <torch/torch.h>
+
+class TetrisModelV1 : torch::nn::Module {
+  private:
+    torch::nn::Linear fc1{nullptr};
+    torch::nn::Linear fc2{nullptr};
+    torch::nn::Linear fc3{nullptr};
+  public:
+    TetrisModelV1();
+    torch::Tensor forward(torch::Tensor);
+};

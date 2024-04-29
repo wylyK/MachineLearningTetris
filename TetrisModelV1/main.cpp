c29e804b5ad557ed67f37d0f76208373f635dca4
Neil Gupta <nogupta@umass.edu>
2024-04-29 18:10:45 -0400

    get code to run

--- TetrisModelV1/main.cpp
+++ TetrisModelV1/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+#include "FeatureExtractor.h"
+#include "TetrisModelV1.h"
+
 int main() {
+    SimplifiedTetris::Game game(5);
+    std::vector<std::tuple<int, int, int>> placements = game.getPlacements();
+    for (std::tuple<int, int, int> & placement : placements) {
+        auto const newBoard = game.previewMove(std::get<0>(placement), std::get<1>(placement),std::get<2>(placement));
+        std::cout << std::endl;
+        newBoard->print();
+        std::cout << std::endl << "holes:" << getNumHoles(game.getBoard()) << std::endl;
+        std::cout << std::endl << "wells:" << getNumWells(game.getBoard()) << std::endl << std::endl;
+    }
+
     std::cout << "Hello" << std::endl;
 
 //    SimplifiedTetris::Game game(5);

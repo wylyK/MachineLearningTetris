8f64a8016be486f23cf51282c6ffdbb77e028a75
Neil Gupta <nogupta@umass.edu>
2024-04-30 17:05:43 -0400

    make struct

--- TetrisModelV1/TetrisModelV1.h
+++ TetrisModelV1/TetrisModelV1.h
@@ -1,11 +1,9 @@
 #include <torch/torch.h>
 
-class TetrisModelV1 : torch::nn::Module {
-  private:
+struct TetrisModelV1 : torch::nn::Module {
     torch::nn::Linear fc1{nullptr};
     torch::nn::Linear fc2{nullptr};
     torch::nn::Linear fc3{nullptr};
-  public:
     TetrisModelV1();
     torch::Tensor forward(torch::Tensor);
 };
--- TetrisModelV1/main.cpp
+++ TetrisModelV1/main.cpp
@@ -28,10 +28,10 @@ int main() {
 //        torch::data::datasets::MNIST("./data").map(
 //            torch::data::transforms::Stack<>()),
 //        /*batch_size=*/64);
-//
-//    // Instantiate an SGD optimization algorithm to update our Net's parameters.
-//    torch::optim::SGD optimizer(net->parameters(), /*lr=*/0.01);
-//
+
+    // Instantiate an SGD optimization algorithm to update our Net's parameters.
+    torch::optim::SGD optimizer(net->parameters(), /*lr=*/0.01);
+
 //    for (size_t epoch = 1; epoch <= 10; ++epoch) {
 //        size_t batch_index = 0;
 //        // Iterate the data loader to yield batches from the dataset.

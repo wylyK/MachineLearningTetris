06eb02a8e2ee06718df1921817930ac3de6a6956
Neil Gupta <nogupta@umass.edu>
2024-05-01 18:16:12 -0400

    calculate num parameters

--- TetrisModelV1/TetrisModelV1.cpp
+++ TetrisModelV1/TetrisModelV1.cpp
@@ -1,9 +1,9 @@
 #include "TetrisModelV1.h"
 
 TetrisModelV1::Net::Net() {
-    fc1 = register_module("fc1", torch::nn::Linear(19, 10));
-    fc2 = register_module("fc2", torch::nn::Linear(10, 10));
-    fc3 = register_module("fc3", torch::nn::Linear(10, 1));
+    fc1 = register_module("fc1", torch::nn::Linear(INPUT_FEATURES, MIDDLE_LAYER_1_SIZE));
+    fc2 = register_module("fc2", torch::nn::Linear(MIDDLE_LAYER_1_SIZE, MIDDLE_LAYER_2_SIZE));
+    fc3 = register_module("fc3", torch::nn::Linear(MIDDLE_LAYER_2_SIZE, 1));
 }
 
 torch::Tensor TetrisModelV1::Net::forward(torch::Tensor x) {
@@ -15,10 +15,14 @@ torch::Tensor TetrisModelV1::Net::forward(torch::Tensor x) {
 
 void TetrisModelV1::setParams(int const param) {
     int i = 0;
+    int64_t totalParams = 0;
     for (torch::Tensor & t : net.parameters()) {
         std::cout << "params " << i << ": " << t << std::endl;
+        totalParams += t.numel();
         ++i;
     }
+    std::cout << totalParams << " total parameters counted" << std::endl;
+    std::cout << NUM_PARAMETERS << " total parameters calculated" << std::endl;
 }
 
 torch::Tensor TetrisModelV1::evaluate(torch::Tensor const & features) {
--- TetrisModelV1/TetrisModelV1.h
+++ TetrisModelV1/TetrisModelV1.h
@@ -2,6 +2,19 @@
 
 class TetrisModelV1 {
   public:
+    static size_t constexpr INPUT_FEATURES = 19;
+    static size_t constexpr MIDDLE_LAYER_1_SIZE = 10;
+    static size_t constexpr MIDDLE_LAYER_2_SIZE = 10;
+
+    static size_t constexpr NUM_PARAMETERS = (
+        INPUT_FEATURES * MIDDLE_LAYER_1_SIZE  // middle layer 1 weights
+        + MIDDLE_LAYER_1_SIZE  // middle layer 1 biases
+        + MIDDLE_LAYER_1_SIZE * MIDDLE_LAYER_2_SIZE  // middle layer 2 weights
+        + MIDDLE_LAYER_2_SIZE  // middle layer 2 biases
+        + MIDDLE_LAYER_2_SIZE  // output layer weights
+        + 1 // output layer bias
+    );
+
     struct Net : torch::nn::Module {
         torch::nn::Linear fc1{nullptr};
         torch::nn::Linear fc2{nullptr};

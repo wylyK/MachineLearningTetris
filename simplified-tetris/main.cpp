f73f98cae6a522eff211f3079f1226efd3dc8649
Neil Gupta <nogupta@umass.edu>
2024-05-02 19:26:12 -0400

    run game

--- simplified-tetris/main.cpp
+++ simplified-tetris/main.cpp
@@ -3,20 +3,17 @@
 
 int main() {
     SimplifiedTetris::Game game(5);
-    std::cout << (int)game.getFalling() << std::endl;
     game.printBoard();
-    std::cout << std::endl << (int)game.getFalling() << std::endl << std::endl;
-    std::vector<SimplifiedTetris::Move> placements = game.getPlacements();
-//    for (std::tuple<int, int, int> & placement : placements) {
-//        std::cout << std::get<0>(placement) << ", " << std::get<1>(placement) << ", " << std::get<2>(placement) << std::endl;
-//    }
-
-    game.doMove(placements[0]);
-    game.getBoard().print();
-
-    placements = game.getPlacements();
-    game.doMove(placements[0]);
-    game.getBoard().print();
-
+    std::cout << std::endl;
+    while (true) {
+        auto placements = game.getPlacements();
+        if (placements.size() == 0) {
+            std::cout << "out of moves" << std::endl;
+            break;
+        }
+        game.doMove(placements[0]);
+        game.getBoard().print();
+        std::cout << std::endl;
+    }
 
 }

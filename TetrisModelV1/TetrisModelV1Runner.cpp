6dd7717706b6f284ec785ca8dcc46f3db2bca091
Neil Gupta <nogupta@umass.edu>
2024-05-03 12:57:02 -0400

    initialize weights

--- TetrisModelV1/TetrisModelV1Runner.cpp
+++ TetrisModelV1/TetrisModelV1Runner.cpp
@@ -2,4 +2,8 @@
 
 TetrisModelV1Runner::TetrisModelV1Runner(TetrisModelV1Runner::seed_type const seed) :
     game(seed)
-{}
+{
+    // mean 0, stddev .5
+    weights = torch::normal(0, .5, TetrisModelV1::NUM_PARAMETERS);
+    model.setParams(weights);
+}
--- TetrisModelV1/TetrisModelV1Runner.h
+++ TetrisModelV1/TetrisModelV1Runner.h
@@ -1,5 +1,7 @@
 #ifndef SIMPLIFIED_TETRIS_TETRISMODELV1RUNNER_H
 #define SIMPLIFIED_TETRIS_TETRISMODELV1RUNNER_H
+#include <torch/torch.h>
+
 #include "TetrisModelV1.h"
 #include "../simplified-tetris/SimplifiedTetris.h"
 
@@ -7,6 +9,7 @@ class TetrisModelV1Runner {
   private:
     TetrisModelV1 model;
     SimplifiedTetris::Game game;
+    torch::Tensor weights;
   public:
     typedef SimplifiedTetris::Game::seed_type seed_type;
     explicit TetrisModelV1Runner(seed_type);

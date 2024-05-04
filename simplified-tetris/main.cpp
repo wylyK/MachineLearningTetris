d8d9d9b006786eb7620bc1e1013b31519d491f19
Neil Gupta <nogupta@umass.edu>
2024-05-03 22:08:13 -0400

    manually play to clear row

--- simplified-tetris/main.cpp
+++ simplified-tetris/main.cpp
@@ -5,13 +5,14 @@ int main() {
     SimplifiedTetris::Game game(5);
     game.printBoard();
     std::cout << std::endl;
-    while (true) {
+    int moves[] = {0, 3, 8, 6, 8, 5};
+    for (int const & i : moves) {
         auto placements = game.getPlacements();
         if (placements.empty()) {
             std::cout << "out of moves" << std::endl;
             break;
         }
-        game.doMove(placements[0]);
+        game.doMove(placements[i]);
         game.getBoard().print();
         std::cout << std::endl;
     }
